eba6315059e2a3ef9a518f107fc31652eaeadb7d
Jungieve <zihan.cong@gmail.com>
2017-03-06 17:41:49 +0800

    微信认证授权并获取accesstoken及用户信息

--- controllers/oauth.js
+++ controllers/oauth.js
@@ -10,15 +10,41 @@ module.exports = {
      * @param next
      */
     getAuthorizeURL: function (req, res, next) {
-        var redirectURL =  encodeUrl(req.params.URL);
-        var url = client.getAuthorizeURL('http://' + redirectURL, '', 'snsapi_userinfo');
+        var redirectUrl = encodeURI('http://jungieve.s1.natapp.cc/oauth/callback');
+        var url = client.getAuthorizeURL(redirectUrl, '','snsapi_userinfo');
+        console.log(url)
         res.redirect(url)
     },
+    /**
+     * 获取授权页面的URL地址(pc端)
+     * @param req
+     * @param res
+     * @param next
+     */
     getAuthorizeURLForWebsite: function (req, res, next) {
-        var redirectURL = encodeUrl(req.params.URL);
-        var url = client.getAuthorizeURLForWebsite('http://' + redirectURL, '', 'snsapi_userinfo');
+        var redirectUrl = encodeURI('http://jungieve.s1.natapp.cc/oauth/callback');
+        var url = client.getAuthorizeURL(redirectUrl, '','snsapi_userinfo');
         res.redirect(url)
     },
-
+    /**
+     * 认证授权的回调函数
+     * @param req
+     * @param res
+     * @param next
+     */
+    callback:function (req, res, next) {
+        console.log('a')
+        var code = req.query.code;
+        client.getAccessToken(code, function (err, result) {
+            var accessToken = result.data.access_token;
+            var openid = result.data.openid;
+            console.log('当前token=' + accessToken);
+            console.log('当前openid=' + openid);
+            client.getUser(openid, function (err, result) {
+                console.log('use weixin api get user: ' + err);
+                console.log(result);
+            })
+        })
+    }
 
 }
--- routes/oauth.js
+++ routes/oauth.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var oauthController = require('../controllers/oauth');
 
-router.get('/:URL', oauthController.getAuthorizeURL);
-router.get('/:URLForWebsite', oauthController.getAuthorizeURLForWebsite);
+router.get('/mobile', oauthController.getAuthorizeURL);
+router.get('/pc', oauthController.getAuthorizeURLForWebsite);
+router.get('/callback', oauthController.callback);
 
 module.exports = router;
