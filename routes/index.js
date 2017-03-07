fba261a0733ec7eaa12a3b1a6428eee88c61eac6
Jungieve <zihan.cong@gmail.com>
2017-03-07 21:13:36 +0800

    七牛文件上传与数据库的对接

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,8 @@ var express = require('express');
 var router = express.Router();
 var indexController = require('../controllers/index');
 
+/**
+ * 微信签名认证
+ */
 router.get('/',indexController.checkSignature);
 module.exports = router;
--- routes/wechat.js
+++ routes/wechat.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 var oauthController = require('../controllers/wechat');
 
+/**
+ * 微信授权并获取用户信息
+ */
 router.get('/authorize/info', oauthController.getAuthorizedInfo);
+/**
+ * 静默授权并获取用户信息（可能部分）
+ */
 router.get('/info', oauthController.getUserInfo);
+/**
+ * 微信回调方法
+ */
 router.get('/callback', oauthController.authorizeCallback);
 
 module.exports = router;
