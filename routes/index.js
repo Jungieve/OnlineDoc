ff7dca4f1495d5673e767567da0400a2adfb1da9
Jungieve <zihan.cong@gmail.com>
2017-03-29 00:11:16 +0800

    增加ejs的路由

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var indexController = require('../controllers/index');
 
+router.get('/test', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
 router.get('/',indexController.checkSignature);
 router.post('/', indexController.checkSignature);
 module.exports = router;
