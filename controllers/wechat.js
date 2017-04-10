4b03e8f7e81e30bd5bd68125136b3d4f127e1e8f
Jungieve <zihan.cong@gmail.com>
2017-04-10 23:48:52 +0800

    增加微信扫码登录接口

--- controllers/wechat.js
+++ controllers/wechat.js
@@ -29,6 +29,17 @@ module.exports = {
         res.redirect(url)
     },
     /**
+     * 查询用户信息,如果授权返回全部信息，否则返回openid
+     * @param req
+     * @param res
+     * @param next
+     */
+    getAuthorizedForWebsite: function (req, res, next) {
+        var redirectUrl = encodeURI('http://' + domainConfig.domain + '/wechat/callback');
+        var url = client.getAuthorizeURLForWebsite(redirectUrl, 'login', 'snsapi_login');
+        res.redirect(url)
+    },
+    /**
      * 认证授权的回调函数
      * @param req
      * @param res
@@ -49,9 +60,9 @@ module.exports = {
                     // 如果未找到用户且未授权， 返回openid; 否则返回用户个人信息
                     if(state == 'base')
                         res.json(openid);
-                    if(state == 'userinfo') {
+                    if (state == 'userinfo' || state == 'login') {
                         client.getUser(openid, function (err, result) {
-                            console.log('采用微信API获得user');
+                            console.log('采用微信API获得user,途径为' + state);
                             console.log('error: ' + err)
                             var oauth_user = result;
                             result = new userModel(oauth_user);
